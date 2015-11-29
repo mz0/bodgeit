39f1177092df8ca97c6a73b04fb3c78ee304c5fe
Mark Zhitomirski <marcuzero@gmail.com>
2015-11-29 20:56:47 +0300

    tidy up
    - add 'includes' so javac task should recompile only when src/com
      is newer then 'classes/'
    - change package name to 'check'

--- src/check/FunctionalTest.java
+++ src/check/FunctionalTest.java
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-package test;
+package check;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
